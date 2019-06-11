734807912b646540fe5309024dc5166176fc6440
chris rivas <irivas@ucol.mx>
2019-06-11 13:29:31 -0500

    si presiona enter la variable opciones cambia a 1

--- timer.c
+++ timer.c
@@ -14,12 +14,17 @@ void main()
    enable_interrupts(GLOBAL);
    set_timer0(26473.5); //a
    char opcion;
-   printf("Hola examen, Pulse R para poner contador a 0, S empieze a contar A para y enviar contador actual");
+   int opciones=0;
+   printf("Hola examen, Pulse R para poner contador a 0, S empieze a contar A para y enviar contador actual: \r");
    while(TRUE)
    {
     if(kbhit()){            //01
         opcion=getch();//-+//+
         if(opcion==65 || opcion<=83 || opcion==82||opcion==13){
+           printf("%c",opcion);
+        }
+        if(opcion==13){
+            opciones=1;
         }
     }
       //TODO: User Code
