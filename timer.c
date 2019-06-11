a49ca81644a2c97c8c94a5ecc760d48286944e28
chris rivas <irivas@ucol.mx>
2019-06-11 13:11:52 -0500

    se activan las interrupciones globales y timer0 con valor 2673.5 para 1 segundo

--- timer.c
+++ timer.c
@@ -2,12 +2,15 @@
 
 void main()
 {
+   setup_timer_0(RTCC_INTERNAL | RTCC_DIV_256 );
+   enable_interrupts(INT_TIMER0);
+   enable_interrupts(GLOBAL);
+   set_timer0(26473.5); //a
+   while(TRUE)
+   {
 
-	while(TRUE)
-	{
 
-
-		//TODO: User Code
-	}
+      //TODO: User Code
+   }
 
 }
