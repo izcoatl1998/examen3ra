6dbff3a0f6c9890d69cb1fe6d7e255cdf9a3141c
chris rivas <irivas@ucol.mx>
2019-06-11 13:15:42 -0500

    mandamos un saludo y señalando las opciones que hay

--- timer.c
+++ timer.c
@@ -6,9 +6,11 @@ void main()
    enable_interrupts(INT_TIMER0);
    enable_interrupts(GLOBAL);
    set_timer0(26473.5); //a
+   flag_pausa=0;
+   printf("Hola examen, Pulse R para poner contador a 0, S empieze a contar A para y enviar contador actual");
    while(TRUE)
    {
-
+    
 
       //TODO: User Code
    }
--- timer.h
+++ timer.h
@@ -1,8 +1,7 @@
 #include <18F4620.h>
 #device ADC=10
 
-#FUSES NOWDT                 	//No Watch Dog Timer
+#FUSES NOWDT                    //No Watch Dog Timer
 
 #use delay(crystal=16MHz)
-#use rs232(baud=9600,parity=N,xmit=None,rcv=None,bits=8,stream=PORT1)
-
+#use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8,stream=PORT1)   
