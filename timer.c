026d1e4c1e819ab62125c595cfc6708236bb74a3
chris rivas <irivas@ucol.mx>
2019-06-11 13:22:48 -0500

    se configura la interrupcion para cuando la flag_opcion valga 1 empieze a contar

--- timer.c
+++ timer.c
@@ -1,12 +1,18 @@
 #include <timer.h>
-
+int contador=0;
+int flag_opcion=0;
+void TIMER0(){
+ if(flag_opcion==1){
+ contador++;}
+set_timer0(26473.5);
+}
 void main()
 {
    setup_timer_0(RTCC_INTERNAL | RTCC_DIV_256 );
    enable_interrupts(INT_TIMER0);
    enable_interrupts(GLOBAL);
    set_timer0(26473.5); //a
-   int flag_pausa=0;
+  
    printf("Hola examen, Pulse R para poner contador a 0, S empieze a contar A para y enviar contador actual");
    while(TRUE)
    {
