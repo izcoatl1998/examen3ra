8bb6aacd7af596b6f249faf83f61bffe91b047aa
chris rivas <irivas@ucol.mx>
2019-06-11 13:32:11 -0500

    creo varibale contador led para cada segundo se apague el led

--- timer.c
+++ timer.c
@@ -1,10 +1,12 @@
 #include <timer.h>
 int contador=0;
 int flag_opcion=0;
+int contador_led=0;
 #INT_TIMER0
 void TIMER0(){
  if(flag_opcion==1){
  contador++;}
+ output_low(PIN_D2);
 set_timer0(26473.5);
 }
 void main()
@@ -18,6 +20,9 @@ void main()
    printf("Hola examen, Pulse R para poner contador a 0, S empieze a contar A para y enviar contador actual: \r");
    while(TRUE)
    {
+    if(contador_led==1){
+    output_high(PIN_D2);
+    contador_led=0;}
     if(kbhit()){            //01
         opcion=getch();//-+//+
         if(opcion==65 || opcion<=83 || opcion==82||opcion==13){
